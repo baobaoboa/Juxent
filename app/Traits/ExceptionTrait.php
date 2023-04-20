a41b43ea7c97694aa61aac9e29c9f39cb15f4855
James Francis <49358080+gfifhy@users.noreply.github.com>
2023-04-20 13:19:47 +0800

    Hide Password on login return

--- app/Traits/ExceptionTrait.php
+++ app/Traits/ExceptionTrait.php
@@ -4,7 +4,7 @@ namespace App\Traits;
 use Illuminate\Validation\ValidationException;
 
 trait ExceptionTrait {
-    private function throwException($message, $errorCode) {
+    private function throwException($errorCode,$message) {
         throw ValidationException::withMessages([
             'code' => $errorCode,
             'message' => $message
