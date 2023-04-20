90ee852647ede9e1c4d16966a1e6ab41c4a8fc37
James Francis <49358080+gfifhy@users.noreply.github.com>
2023-04-20 08:11:55 +0800

    added user and roles seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call(EmployeeRolesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
