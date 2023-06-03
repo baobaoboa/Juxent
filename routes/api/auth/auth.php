5600b1dca4719c9b92d4aa91e00a9234647af9dc
James Francis <49358080+gfifhy@users.noreply.github.com>
2023-06-04 04:26:46 +0800

    added contact in client

--- routes/api/auth/auth.php
+++ routes/api/auth/auth.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\IssueController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ResourceController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WarrantyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
